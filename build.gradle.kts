faeba0d2a21a2757f24ad2ad1039669a192b048c
Ovidiu Luca <ov.luca@gmail.com>
2025-11-02 17:42:17 +0200

    Add database dependencies and Flyway plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.0"
     id("io.ktor.plugin") version "3.0.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+    id("org.flywaydb.flyway") version "10.10.0"
     application
 }
 
@@ -20,5 +21,14 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor")
     implementation("io.ktor:ktor-server-websockets:$ktor")
     implementation("io.ktor:ktor-server-swagger:$ktor")
+
+    // Database dependencies
+    implementation("org.jetbrains.exposed:exposed-core:0.55.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.55.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.55.0")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("org.postgresql:postgresql:42.7.4")
+    implementation("org.flywaydb:flyway-core:10.10.0")
+
     testImplementation(kotlin("test"))
 }
